08c1dbc7ef20ecc341af93becf9abd0c63780d1e
Adam Hudak <adamhudak1@proton.me>
2024-04-10 22:54:51 -0500

    add Sidebar, Navbar, Clerk

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    imgUrl: "/icons/HOME.svg",
+    route: "/",
+  },
+  {
+    label: "Upcoming",
+    imgUrl: "/icons/upcoming.svg",
+    route: "/upcoming",
+  },
+  {
+    label: "Previous",
+    imgUrl: "/icons/previous.svg",
+    route: "/previous",
+  },
+  {
+    label: "Recordings",
+    imgUrl: "/icons/Video.svg",
+    route: "/recordings",
+  },
+  {
+    label: "Personal room",
+    imgUrl: "/icons/add-personal.svg",
+    route: "/personal-room",
+  },
+];
